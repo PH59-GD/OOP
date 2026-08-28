3e5ec8fe76504f0e5f2e91f750243eb744281e9e
PH59-GD <hammelpayton@gmail.com>
2026-08-28 09:51:00 -0500

    Add files via upload

--- PC2.py
+++ PC2.py
@@ -1,11 +1,12 @@
-Number1 = 10
-Number2 = 20
+Number1 = int(input("Enter Number1: "))
+Number2 = int(input("Enter Number2: "))
+Number3 = int(input("Enter Number3: "))
 
-if Number1 > Number2:
-    print("Number1 is the biggest.")
-elif Number1 != Number2:
-    print("They are not equal.")
-elif Number1 < Number2:
-    print("Number2 is the biggest.")
-else:
-    print("Invalid numbers.")
+if Number1 > Number2 and Number1 > Number3:
+    print("Number1 is the biggest")
+
+if Number2 > Number3 and Number2 > Number1:
+    print("Number2 is the biggest")
+
+if Number3 > Number1 and Number3 > Number2:
+    print("Number3 is the biggest")
