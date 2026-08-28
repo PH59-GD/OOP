20c429004abe9e2ecbc7a890f47afe8ea79ede79
PH59-GD <hammelpayton@gmail.com>
2026-08-28 09:23:19 -0500

    Add files via upload

---
+++ PC2.py
@@ -0,0 +1,11 @@
+Number1 = 10
+Number2 = 20
+
+if Number1 > Number2:
+    print("Number1 is the biggest.")
+elif Number1 != Number2:
+    print("They are not equal.")
+elif Number1 < Number2:
+    print("Number2 is the biggest.")
+else:
+    print("Invalid numbers.")
---
+++ main.py
@@ -0,0 +1,9 @@
+print("Enter a number for A")
+a = int(input())
+
+print("Enter a nnumber for B")
+b = int(input())      # <--- TypeCasting
+
+c = a + b
+
+print("The summation of a and b =:", c)
