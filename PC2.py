675438666f6473195e8a028caa443b64770dda8c
PH59-GD <hammelpayton@gmail.com>
2026-08-30 19:56:25 -0500

    Add files via upload

--- PC2.py
+++ PC2.py
@@ -1,3 +1,4 @@
+import time
 Number1 = int(input("Enter Number1: "))
 Number2 = int(input("Enter Number2: "))
 Number3 = int(input("Enter Number3: "))
@@ -5,8 +6,19 @@ Number3 = int(input("Enter Number3: "))
 if Number1 > Number2 and Number1 > Number3:
     print("Number1 is the biggest")
 
-if Number2 > Number3 and Number2 > Number1:
+elif Number2 > Number3 and Number2 > Number1:
     print("Number2 is the biggest")
 
-if Number3 > Number1 and Number3 > Number2:
+elif Number3 > Number1 and Number3 > Number2:
     print("Number3 is the biggest")
+
+if Number1 < Number2 and Number1 < Number3:
+    print("Number1 is the smallest")
+
+elif Number2 < Number3 and Number2 < Number1:
+    print("Number2 is the smallest")
+
+elif Number3 < Number1 and Number3 < Number2:
+    print("Number3 is the smallest")
+
+time.sleep(60)
