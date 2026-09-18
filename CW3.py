723b7be2b378698e59d61dbf8b49d9d373ee269b
PH59-GD <hammelpayton@gmail.com>
2026-09-18 08:34:42 -0500

    Add files via upload

--- CW3.py
+++ CW3.py
@@ -1,5 +1,5 @@
 Courses = {}
-
+L = []
 while 1:
     print("")
     print("[1] Add a course")
@@ -8,10 +8,33 @@ while 1:
     print("")
     UI = input("Input: ")
     if (UI == "1"):
+        print("")
         UA = input("Name of course to add: ")
-        Courses["Course"+str(len(Courses)+1)] = [UA]
+        Courses["Course"+str(len(Courses)+1)] = UA
+        L += UA
         print(Courses)
-    if (UI == "2"):
-        print(Courses.keys())
+        print(L)
+    elif (UI == "2"):
+        print("")
+        print(Courses)
+        print("")
         UR = input("What course to remove?: ")
-        
+        for i in range(len(L)):
+            if L[i - 1] == Courses[UR]:
+                del L[i - 1]
+                print(L)
+        del Courses[UR]
+        Courses = {}
+        for b in range(len(L)):
+            Courses["Course"+str(b+1)] = L[b]
+        print(Courses)
+    elif (UI == "3"):
+        print("")
+        print(Courses)
+        print("")
+        UR = input("What course to replace?: ")
+        NC = input("What course to replace it with?: ")
+        if UR in Courses:
+            Courses[UR] = NC
+        else:
+            print("Course not in dictonary!")
